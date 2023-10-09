b66b7132c59df6507c41ea95c61fedb4486ec6f3
leve68 <hanstom000608@gmail.com>
2023-10-09 23:35:26 +0900

    Main UI

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+
+    protected override void Init()
+    {
+        base.Init();
+        Managers managers = Managers.s_managersProperty;
+        Managers.uiManagerProperty.ShowSceneUI<UI_Game>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     public override void Clear()
     {
         
