a154fe253781bf2512a192f30e83313984abedeb
raccoonD0G <w3yfrl@naver.com>
2023-10-09 20:50:28 +0900

    10/09/20:50

--- Assets/Scripts/Scenes/LobbyScene.cs
+++ Assets/Scripts/Scenes/LobbyScene.cs
@@ -9,10 +9,16 @@ public class LobbyScene : BaseScene
         throw new System.NotImplementedException();
     }
 
+    protected override void Init()
+    {
+        base.Init();
+        Managers managers = Managers.s_managersProperty;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scenes/RecipeBookScene.cs
+++ Assets/Scripts/Scenes/RecipeBookScene.cs
@@ -9,10 +9,17 @@ public class RecipeBookScene : BaseScene
         throw new System.NotImplementedException();
     }
 
+    protected override void Init()
+    {
+        base.Init();
+        Managers managers = Managers.s_managersProperty;
+        Managers.uiManagerProperty.ShowSceneUI<UI_RecipeBook>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
     }
 
     // Update is called once per frame
